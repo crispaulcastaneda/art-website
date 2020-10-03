9a460fecc0a835a1152503270e3af241c6d4b262
crispaulcastaneda <cristianpaul_c@yahoo.com>
2020-10-03 19:23:11 +0800

    updates

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,3 +33,21 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(type, insertTxtDelay + 150);
 })
+
+       //  SWIPER
+       var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+        },
+        pagination: {
+        el: '.swiper-pagination',
+        },
+    });
